876b8a0e0aa5a493cb1f5b530e5b67e5cd9972cf
rajnikant <59799061+automation555@users.noreply.github.com>
2021-10-18 17:35:38 +0530

    Update complex_main.cpp

--- tests/complex_main.cpp
+++ tests/complex_main.cpp
@@ -6,7 +6,6 @@ int main() {
 	Complex a(3,5);
 	Complex b(3,0);
 	Complex c(a);
-
 	std::cout << a.getReal() << " " << a.getImag() << " " << a.abs() << std::endl;
 	std::cout << b.getReal() << " " << b.getImag() << " " << b.abs() << std::endl;
 	std::cout << c.getReal() << " " << c.getImag() << " " << c.abs() << std::endl; 
